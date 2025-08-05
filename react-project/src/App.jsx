52dc4e1c8f175c7bd7682d5c124e3ef0421b285f
Renato Leite <dangelrenato@gmail.com>
2025-08-05 15:13:35 +0000

    fix

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,12 +1,53 @@
-import './App.css'
+import './App.css';
+import chef from "./images/chef.jpg";
 
-let language = "React";
+function Header({name, year}) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>  
+  );
+}
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+function Main({dishes}) {
+  return (
+    <>
+      <div>
+        <h2>Welcome to this beautiful restaurant!</h2>
+      </div>
+      <main>
+        <img src={chef} height={200} alt="A photo of a smiling chef owner"/>
+        <ul>
+          {dishes.map((dish) => (
+            <li key= {dish.id} style= {{listStyleType: "none"}}>{dish.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  )
+}
 
 function App() {
 
-  return (
-    <h1>Hello {language.toUpperCase()}!!</h1>
-  )
+  return(
+  <div>
+    <Header name="Alex" year={new Date().getFullYear()} />
+    <Main dishes = {dishObjects}/>
+  </div>);
+  
 }
 
 export default App
